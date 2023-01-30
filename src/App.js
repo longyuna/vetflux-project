df33e6c1b9f059d5d839fd3b4a56a3101a8f10fc
longYuna <longyuna@longYunaui-MacBookPro.local>
2023-01-30 13:46:39 +0900

    Add: 중복제거 및 유효성 검사 추가

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import * as xlsx from "xlsx";
 import "./App.css";
 
 const App = () => {
-  const [data, setData] = useState([]);
+  const [validData, setValidData] = useState([]);
+  const [inValidData, setInValidData] = useState([]);
+  console.log(validData);
+  console.log(inValidData);
 
   const readUploadFile = (e) => {
+    const phoneRegex =
+      /^(01[016789]{1}|070|02|0[3-9]{1}[0-9]{1})-[0-9]{3,4}-[0-9]{4}$/;
+    const ids = {};
+    const valid = [];
+    const invalid = [];
+
     e.preventDefault();
     if (e.target.files) {
       const reader = new FileReader();
@@ -15,7 +24,52 @@ const App = () => {
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const json = xlsx.utils.sheet_to_json(worksheet);
-        setData(json);
+
+        json.forEach((item) => {
+          const id = item.동물번호 || item.환자ID;
+          const petName = item.동물이름;
+          const name = item.보호자이름;
+          const phone = item.핸드폰번호 || item.휴대폰번호 || item.집전화번호;
+          if (!ids[id]) {
+            ids[id] = true;
+            let gender = "기타";
+            // eslint-disable-next-line default-case
+            switch (item.성별) {
+              case "Canine":
+                gender = "남자";
+                break;
+              case "Feline":
+                gender = "여자";
+                break;
+            }
+            if (
+              !id ||
+              !phone ||
+              !phoneRegex.test(phone) ||
+              !name ||
+              !petName ||
+              !gender
+            ) {
+              invalid.push({
+                id: id,
+                petName: petName,
+                name: name,
+                type: gender,
+                phoneNumber: phone,
+              });
+            } else {
+              valid.push({
+                id: id,
+                petName: petName,
+                name: name,
+                type: gender,
+                phoneNumber: phone,
+              });
+            }
+          }
+        });
+        setValidData(valid);
+        setInValidData(invalid);
       };
       reader.readAsArrayBuffer(e.target.files[0]);
     }
@@ -36,24 +90,50 @@ const App = () => {
         <table>
           <thead>
             <tr>
-              <th>firstName</th>
-              <th>lastName</th>
-              <th>email</th>
-              <th>gender</th>
+              <th>고객id</th>
+              <th>동물이름</th>
+              <th>보호자 이름</th>
+              <th>성별</th>
+              <th>번호</th>
             </tr>
           </thead>
-          <thead>
-            {data.map((userInfo) => {
+          <tbody>
+            {validData.map((item) => {
               return (
-                <tr key={userInfo.id}>
-                  <td>{userInfo.first_name}</td>
-                  <td>{userInfo.last_name}</td>
-                  <td>{userInfo.email}</td>
-                  <td>{userInfo.gender}</td>
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.petName}</td>
+                  <td>{item.name}</td>
+                  <td>{item.type}</td>
+                  <td>{item.phoneNumber}</td>
                 </tr>
               );
             })}
+          </tbody>
+        </table>
+        <table>
+          <thead>
+            <tr>
+              <th>잘못된고객id</th>
+              <th>동물이름</th>
+              <th>보호자 이름</th>
+              <th>성별</th>
+              <th>번호</th>
+            </tr>
           </thead>
+          <tbody>
+            {inValidData.map((item) => {
+              return (
+                <tr key={item.id}>
+                  <td>{item.id}</td>
+                  <td>{item.petName}</td>
+                  <td>{item.name}</td>
+                  <td>{item.type}</td>
+                  <td>{item.phoneNumber}</td>
+                </tr>
+              );
+            })}
+          </tbody>
         </table>
       </div>
     </div>
